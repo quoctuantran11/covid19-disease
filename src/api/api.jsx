3f4dc902cdaaf0104eda5f3ded772a3318864b59
QuocTuan18CLC1 <QuocTuan18CLC1>
2021-11-02 16:38:53 +0700

    add readme + package

---
+++ src/api/api.jsx
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from "react";
+
+function API() {
+    fetch('https://pomber.github.io/covid19/timeseries.json')
+
+
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+  
+export default API;
+
+
---
+++ src/components/Covid.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import API from "../api/api";
+
+function Covid() {
+    return (
+      <div className="body-container">
+        abcd
+      </div>
+    );
+  }
+  
+export default Covid;
